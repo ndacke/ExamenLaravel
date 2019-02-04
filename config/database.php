2cd92724c2a3bc8c22c49a7fb112cd2c8cbdbe76
Ndacke <ndackeg@gmail.com>
2019-02-04 23:41:59 +0000

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
-$DATABASE_URL=parse_url(' postgres://rrzoklkxcfneyv:510b1b2e56b
-9ba4b99bf78d2f546f17536eb4a90490ad4e303d07eeaebc91e10@ec2-174
--129-224-157.compute-1.amazonaws.com:5432/da12nolrpfi359');
+
 return [
 
     /*
@@ -15,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,15 +58,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
